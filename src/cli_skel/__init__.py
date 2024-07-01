c26d878bf848c8ffce29bb8d85e70e0204321be7
Michael Frank <frankm@post.bgu.ac.il>
2024-07-01 09:39:00 +0300

    add imports to __init__

--- src/cli_skel/__init__.py
+++ src/cli_skel/__init__.py
@@ -1,5 +1,5 @@
 import importlib.metadata
-# import logging
+import logging
 import pathlib
 
 
@@ -15,9 +15,14 @@ __package_root__ = pathlib.Path(__file__).parent.parent
 """ Path of package root in filesystem -- read only value """
 
 
-# __package_logger_name__ = "cli_skel"
-# """ Name of default package logger -- read only value """
-#
-#
-# __package_default_log_level__ = logging.WARNING
-# """ Default log level for package logger -- read only value """
+__package_logger_name__ = "cli_skel"
+""" Name of default package logger -- read only value """
+
+
+__package_default_log_level__ = logging.WARNING
+""" Default log level for package logger -- read only value """
+
+
+from . import argparse_skel
+from . import cmd_skel
+from . import utils
